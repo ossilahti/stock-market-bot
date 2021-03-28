ffae827489039239fe07aa9a46459ffa4f8da3e5
ossilahti <ossi.lahti5@gmail.com>
2021-03-28 15:17:57 +0300

    Cleaning up the embed messages

--- bot/stockbot.py
+++ bot/stockbot.py
@@ -1,5 +1,7 @@
 import os
 import discord
+import json
+import re
 from discord import Embed
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -20,13 +22,19 @@ async def on_ready():
 async def f(ctx, *, question):
 
     URL = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={question}&apikey={tokenkey}'
-
     async with request('GET', URL, headers={}) as response:
         if response.status == 200:
             data = await response.json()
-            embed = Embed(title=f'Ticker: {question.upper()}',
-                          description=f"""P/E: {data['PERatio']}\nP/B: {data['PriceToBookRatio']}\nP/S: {data['PriceToSalesRatioTTM']}\nDividend yield: {data['DividendYield']}""",
+            json_string = json.dumps(data['Description'])
+            json_string = ' '.join(re.split(r'(?<=[.])\s', json_string)[:3])
+
+            embed = Embed(title=f"{data['Name']} - ({question.upper()})",
+                          description = json_string[1:],
                           colour = 0x2ecc71)
+                
+            embed.add_field(name = 'Fundamentals', 
+                            value = f"""P/E: {data['PERatio']}\nP/B: {data['PriceToBookRatio']}\nP/S: {data['PriceToSalesRatioTTM']}\nDividend yield: {data['DividendYield']}""", 
+                            inline = True)
             await ctx.send(embed=embed)
         else:
             await ctx.send(f'API Returned a {response.status} status' )
